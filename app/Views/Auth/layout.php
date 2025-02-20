0ab99296c41354246dbe0f28d43de021a54d049b
asif03 <babu033045@gmail.com>
2025-02-20 13:07:51 +0600

    Update Code

--- app/Views/Auth/layout.php
+++ app/Views/Auth/layout.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="shortcut icon" type="image/png" href="<?php echo base_url(); ?>public/favicon.ico" />
 
   <title><?php $this->renderSection('title')?></title>
 
--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -1,73 +1,89 @@
-<?= $this->extend(config('Auth')->views['layout']) ?>
+<?php $this->extend(config('Auth')->views['layout'])?>
 
-<?= $this->section('title') ?><?= lang('Auth.login') ?> <?= $this->endSection() ?>
+<?php $this->section('title')?>BCPS :: <?=lang('Auth.login')?><?php $this->endSection()?>
 
-<?= $this->section('main') ?>
+<?php $this->section('main')?>
 
-<div class="container d-flex justify-content-center p-5">
-    <div class="card col-12 col-md-5 shadow-sm">
-        <div class="card-body">
-            <h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
+<div
+  class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+  <div class="d-flex align-items-center justify-content-center w-100">
+    <div class="row justify-content-center w-100">
+      <div class="col-md-8 col-lg-6 col-xxl-3">
+        <div class="card mb-0">
+          <div class="card-body">
+            <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100 text-decoration-none d-flex
+            align-items-center justify-content-center gap-1">
+              <img src="<?php echo base_url(); ?>public/logo.png" width="40" height="40" alt="BCPS Logo"><span
+                class="fs-3 fw-bold">BCPS</span>
+            </a>
+            <h5 class="text-center mb-4"><?=lang('Auth.login')?></h5>
 
-            <?php if (session('error') !== null) : ?>
-                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-            <?php elseif (session('errors') !== null) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <?php if (is_array(session('errors'))) : ?>
-                        <?php foreach (session('errors') as $error) : ?>
-                            <?= $error ?>
-                            <br>
-                        <?php endforeach ?>
-                    <?php else : ?>
-                        <?= session('errors') ?>
-                    <?php endif ?>
-                </div>
-            <?php endif ?>
+            <?php if (session('error') !== null): ?>
+            <div class="alert alert-danger" role="alert"><?=session('error')?></div>
+            <?php elseif (session('errors') !== null): ?>
+            <div class="alert alert-danger" role="alert">
+              <?php if (is_array(session('errors'))): ?>
+<?php foreach (session('errors') as $error): ?>
+              <?=$error?>
+              <br>
+              <?php endforeach?>
+<?php else: ?>
+              <?=session('errors')?>
+<?php endif?>
+            </div>
+            <?php endif?>
 
-            <?php if (session('message') !== null) : ?>
-                <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-            <?php endif ?>
+            <?php if (session('message') !== null): ?>
+            <div class="alert alert-success" role="alert"><?=session('message')?></div>
+            <?php endif?>
 
-            <form action="<?= url_to('login') ?>" method="post">
-                <?= csrf_field() ?>
-
-                <!-- Email -->
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingEmailInput" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
-                    <label for="floatingEmailInput"><?= lang('Auth.email') ?></label>
-                </div>
-
-                <!-- Password -->
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="floatingPasswordInput" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required>
-                    <label for="floatingPasswordInput"><?= lang('Auth.password') ?></label>
-                </div>
-
+            <form action="<?=url_to('login')?>" method="post">
+              <?=csrf_field()?>
+              <!-- Email -->
+              <div class="mb-3">
+                <label for="floatingEmailInput" class="form-label"><?=lang('Auth.email')?></label>
+                <input type="email" class="form-control" id="floatingEmailInput" name="email" inputmode="email"
+                  autocomplete="email" placeholder="<?=lang('Auth.email')?>" value="<?=old('email')?>" required>
+              </div>
+              <!-- Password -->
+              <div class="mb-4">
+                <label for="floatingPasswordInput" class="form-label"><?=lang('Auth.password')?></label>
+                <input type="password" class="form-control" id="floatingPasswordInput" name="password" inputmode="text"
+                  autocomplete="current-password" placeholder="<?=lang('Auth.password')?>" required>
+              </div>
+              <div class="d-flex align-items-center justify-content-between mb-4">
                 <!-- Remember me -->
                 <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
-                    <div class="form-check">
-                        <label class="form-check-label">
-                            <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')): ?> checked<?php endif ?>>
-                            <?= lang('Auth.rememberMe') ?>
-                        </label>
-                    </div>
+                <div class="form-check">
+                  <input class="form-check-input primary" type="checkbox" name="remember" id="flexCheckChecked"
+                    <?php if (old('remember')): ?> checked<?php endif?>>
+                  <label class="form-check-label text-dark" for="flexCheckChecked">
+                    <?=lang('Auth.rememberMe')?>
+                  </label>
+                </div>
                 <?php endif; ?>
 
-                <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                    <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
-                </div>
+                <?php if (setting('Auth.allowMagicLinkLogins')): ?>
+                <a class="text-primary fw-bold" href="<?=url_to('magic-link')?>"><?=lang('Auth.forgotPassword')?></a>
+                <?php endif?>
+              </div>
+              <button type="submit"
+                class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2 text-white fw-bold fs-6"><?=lang('Auth.login')?></button>
 
-                <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-                    <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
-                <?php endif ?>
+              <div class="d-flex align-items-center justify-content-center">
 
-                <?php if (setting('Auth.allowRegistration')) : ?>
-                    <p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
-                <?php endif ?>
 
+                <?php if (setting('Auth.allowRegistration')): ?>
+                <p class="fs-sm mb-0 fw-bold"><?=lang('Auth.needAccount')?> <a class="text-primary ms-1"
+                    href="<?=url_to('register')?>"><?=lang('Auth.register')?></a></p>
+                <?php endif?>
+              </div>
             </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 
-<?php $this->endSection() ?>
+<?php $this->endSection()?>
